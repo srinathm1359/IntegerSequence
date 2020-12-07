b4bfd31c7df3198de6a9919fe71bc5fbb7e3ca5b
srinathstuycs <smahankali10@stuy.edu>
2020-12-07 08:36:50 -0500

    Added constructor

--- Range.java
+++ Range.java
@@ -1,20 +1,32 @@
 import java.util.NoSuchElementException;
 public class Range implements IntegerSequence{
-  private int start,end,current;
+  public int start,end,current;
 
   //@param start : the starting value (inclusive) which must be <= end.
   //@param end : the ending value which is also inclusive.
-  public Range(int start,  int end){  }
+  public Range(int start,  int end) {
+    this.start = start;
+    this.end = end;
+    this.current = start;
+  }
 
-  public void reset(){  }
-  public int length(){  }
+  public void reset() {
+
+  }
+  public int length() {
+    return 0;
+  }
 
   //When current is no longer a valid element in the range, it should return false.
-  public boolean hasNext(){  }
+  public boolean hasNext() {
+    return false;
+  }
 
   //@throws NoSuchElementException when hasNext() is false.
   //This will return the current value, it will also increase current value by 1.
   //e.g.  if current is 5. This will make current 6, and return 5.
-  public int next(){    }
+  public int next() {
+    return 0;
+  }
 
 }
