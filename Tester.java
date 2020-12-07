a4d4f570199604ee36753185635b143af1460c65
srinathstuycs <smahankali10@stuy.edu>
2020-12-07 08:37:08 -0500

    Tested constructor

--- Tester.java
+++ Tester.java
@@ -1,5 +1,8 @@
 public class Tester {
   public static void main(String[] args) {
-    
+    Range hello = new Range(5,10);
+    //System.out.println(hello.start);
+    //System.out.println(hello.end);
+    //System.out.println(hello.current);
   }
 }
